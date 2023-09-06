79b2843bef024fe10b82b029f1b51b9691c02360
YOGI5903 <121392554+YOGI5903@users.noreply.github.com>
2023-09-06 09:58:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,75 @@
+
+
+import streamlit as st
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+
+from PIL import Image
+import numpy as np
+st.title("Garbage Image Classification")
+
+st.write("classify what type of garbage that is being represented in the image.")
+
+model = load_model("bestmodel.h5")
+labels = {
+        0: 'CupBoard box',
+        1: 'metal',
+        2: 'plastic',
+        3: 'glass',
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+uploaded_file = st.file_uploader(
+    "Upload an image of a glass or paper or plastic waste:", type='jpg'
+)
+predictions=-1
+if uploaded_file is not None:
+    image1 = Image.open(uploaded_file)
+    image1=image.smart_resize(image1,(150,150))
+    img_array = image.img_to_array(image1)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array/255.0
+    predictions = model.predict(img_array)
+    label=labels[np.argmax(predictions)]
+
+
+st.write("### Prediction Result")
+if st.button("Predict"):
+    if uploaded_file is not None:
+        image1 = Image.open(uploaded_file)
+        st.image(image1, caption="Uploaded Image", use_column_width=True)
+        st.markdown(
+            f"<h2 style='text-align: center;'>Image of {label}</h2>",
+            unsafe_allow_html=True,
+        )
+    else:
+        st.write("Please upload file or choose sample image.")
+
+
+st.write("If you would not like to upload an image, you can use the sample image instead:")
+sample_img_choice = st.button("Use Sample Image")
+
+if sample_img_choice:
+    image1 = Image.open("garbage.jpg")
+    image1=image.smart_resize(image1,(150,150))
+    img_array = image.img_to_array(image1)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array/255.0
+    predictions = model.predict(img_array)
+    label=labels[np.argmax(predictions)]
+    image1 = Image.open("garbage.jpg")
+    st.image(image1, caption="Uploaded Image", use_column_width=True)    
+    st.markdown(
+        f"<h2 style='text-align: center;'>{label}</h2>",
+        unsafe_allow_html=True,
+    )
