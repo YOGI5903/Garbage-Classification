cc7600166b1d7c9ca331467a927348a2eadf2cbc
YOGI5903 <121392554+YOGI5903@users.noreply.github.com>
2023-09-06 10:19:49 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ st.write("If you would not like to upload an image, you can use the sample image
 sample_img_choice = st.button("Use Sample Image")
 
 if sample_img_choice:
-    image1 = Image.open("garbage.jpg")
+    image1 = Image.open("Vidrio_256.jpg")
     image1=image.smart_resize(image1,(150,150))
     img_array = image.img_to_array(image1)
     img_array = np.expand_dims(img_array, axis=0)
@@ -69,6 +69,19 @@ if sample_img_choice:
     label=labels[np.argmax(predictions)]
     image1 = Image.open("garbage.jpg")
     st.image(image1, caption="Uploaded Image", use_column_width=True)    
+    st.markdown(
+        f"<h2 style='text-align: center;'>{label}</h2>",
+        unsafe_allow_html=True,
+    )
+    image2 = Image.open("metal_16.jpg")
+    image2=image.smart_resize(image2,(150,150))
+    img_array = image.img_to_array(image2)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array/255.0
+    predictions = model.predict(img_array)
+    label=labels[np.argmax(predictions)]
+    image1 = Image.open("garbage.jpg")
+    st.image(image2, caption="Uploaded Image", use_column_width=True)    
     st.markdown(
         f"<h2 style='text-align: center;'>{label}</h2>",
         unsafe_allow_html=True,
